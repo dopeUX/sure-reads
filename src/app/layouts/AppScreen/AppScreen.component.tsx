88a0cb00eec774949ee5abfcf566a6cc8c3a521b
UXNation <uxnation.inc@gmail.com>
2023-11-13 15:46:12 +0530

    init

---
+++ src/app/layouts/AppScreen/AppScreen.component.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export interface AppScreenProps {
+   
+}
+
+const AppScreen:React.FC<AppScreenProps> = () => {
+  return (
+    <div>AppScreenLayout</div>
+  )
+}
+
+export default AppScreen;
